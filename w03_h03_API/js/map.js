bdc66f89ad79415ad6aa5e45fc9bf4df28fa00f7
frannieyy <frannieyy@gmail.com>
2016-09-20 00:17:27 -0400

    Update map.js

--- w03_h03_API/js/map.js
+++ w03_h03_API/js/map.js
@@ -1,20 +1,15 @@
-/* Pull local Farers market data from the USDA API and display on 
-** Google Maps using GeoLocation or user input zip code. By Paul Dessert
-** www.pauldessert.com | www.seedtip.com
-*/
 
 $(function() {
 	
-		var marketId = []; //returned from the API
-		var allLatlng = []; //returned from the API
-		var allMarkers = []; //returned from the API
-		var marketName = []; //returned from the API
+		var marketId = []; 
+		var allLatlng = []; 
+		var allMarkers = []; 
+		var marketName = []; 
 		var infowindow = null;
 		var pos;
 		var userCords;
 		var tempMarkerHolder = [];
 		
-		//map options
 		var mapOptions = {
 			zoom: 5,
 			center: new google.maps.LatLng(37.09024, -100.712891),
@@ -31,20 +26,20 @@ $(function() {
 
 		};
 	
-	//Adding infowindow option
+
 	infowindow = new google.maps.InfoWindow({
 		content: "holding..."
 	});
 	
-	//Fire up Google maps and place inside the map-canvas div
+
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	//grab form data
-    $('#chooseZip').submit(function() { // bind function to submit event of form
+
+    $('#chooseZip').submit(function() { 
 		
-		//define and set variables
+
 		var userZip = $("#textZip").val();
-		//console.log("This-> " + userCords.latitude);
+
 		
 		var accessURL;
 		
@@ -55,7 +50,7 @@ $(function() {
 		}
 			
 
-			//Use the zip code and return all market ids in area.
+
 			$.ajax({
 				type: "GET",
 				contentType: "application/json; charset=utf-8",
@@ -68,15 +63,12 @@ $(function() {
 						marketName.push(val.marketname);
 					 });
 						
-					//console.log(marketName);
 					
 					var counter = 0;
-					//Now, use the id to get detailed info
 					$.each(marketId, function (k, v){
 						$.ajax({
 							type: "GET",
 							contentType: "application/json; charset=utf-8",
-							// submit a get request to the restful service mktDetail.
 							url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + v,
 							dataType: 'jsonp',
 							success: function (data) {
@@ -85,19 +77,15 @@ $(function() {
 
 								var results = data[key];
 								
-								//console.log(results);
-								
-								//The API returns a link to Google maps containing lat and long. This pulls it apart.
+							
 								var googleLink = results['GoogleLink'];
 								var latLong = decodeURIComponent(googleLink.substring(googleLink.indexOf("=")+1, googleLink.lastIndexOf("(")));
 								
 								var split = latLong.split(',');
-								
-								//covert values to floats, to play nice with .LatLng() below.
+				
 								var latitude = parseFloat(split[0]);
 								var longitude = parseFloat(split[1]);
-								
-								//set the markers.	  
+
 								myLatlng = new google.maps.LatLng(latitude,longitude);
 						  
 								allMarkers = new google.maps.Marker({
@@ -106,21 +94,18 @@ $(function() {
 									title: marketName[counter],
 									html: 
 											'<div class="markerPop">' +
-											'<h1>' + marketName[counter].substring(4) + '</h1>' + //substring removes distance from title
+											'<h1>' + marketName[counter].substring(4) + '</h1>' + 
 											'<h3>' + results['Address'] + '</h3>' +
 											'<p>' + results['Products'].split(';') + '</p>' +
 											'<p>' + results['Schedule'] + '</p>' +
 											'</div>'
 								});
 
-								//put all lat long in array
 								allLatlng.push(myLatlng);
 								
-								//Put the marketrs in an array
 								tempMarkerHolder.push(allMarkers);
 								
 								counter++;
-								//console.log(counter);
 							};
 								
 								google.maps.event.addListener(allMarkers, 'click', function () {
@@ -129,26 +114,20 @@ $(function() {
 								});
 								
 								
-								//console.log(allLatlng);
-								//  Make an array of the LatLng's of the markers you want to show
-								//  Create a new viewpoint bound
 								var bounds = new google.maps.LatLngBounds ();
-								//  Go through each...
 								for (var i = 0, LtLgLen = allLatlng.length; i < LtLgLen; i++) {
-								  //  And increase the bounds to take this point
 								  bounds.extend (allLatlng[i]);
 								}
-								//  Fit these bounds to the map
 								map.fitBounds (bounds);
 								
 										
 							}
 						});
-					}); //end .each
+					}); 
 				}
 			});
 
-        return false; // important: prevent the form from submitting
+        return false; 
     });
 });
 
